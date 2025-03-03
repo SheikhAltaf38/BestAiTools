b00155936c29e63396e21c600a8784dbfe7c0f7e
SheikhAltaf38 <altafsheikh311@gmail.com>
2025-03-03 13:09:08 +0530

    added google verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+         {/* Google Search Console Verification */}
+         <meta name="google-site-verification" content="mML9HB2Ln4KnFkoYnmTaax7xsaNA9-hVVvComtF9Lhs" />
         {/* Meta Tags for SEO */}
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
